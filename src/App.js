29c0f43e1c7f369476f6de27aec8a0ea7ca17084
LeoGeorgeto <138282137+LeoGeorgeto@users.noreply.github.com>
2023-11-15 15:28:19 -0500

    search features implemented

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './pages/Register';  // Make sure the path is correct
 import Login from './pages/Login';  // Make sure the path is correct
 import Dashboard from './pages/Dashboard';  // Make sure the path is correct
+import SearchResults from './pages/SearchResults';
 import EventDetail from './pages/EventDetail';
 import Confirmation from "./pages/Confirmation";  // Make sure the path is correct
-import Events from "./pages/Events";
 import RegisterAdmin from "./pages/RegisterAdmin";
 import AdminDashboard from "./pages/AdminDashboard";
 import { AuthProvider } from './components/AuthContext';
@@ -18,12 +18,12 @@ function App() {
                     <Route path="/register" element={<Register />} />
                     <Route path="/register/admin" element={<RegisterAdmin />} />
                     <Route path="/login" element={<Login />} />
-                    <Route path="/" element={<Events />} />
                     <Route path="/adminDashboard" element={<AdminDashboard />} />
                     <Route path="/event/:id" element={<EventDetail />} />
                     <Route path="/dashboard" element={<Dashboard />} />
                     <Route path="/confirmation" element={<Confirmation />} />
-
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/search-results" element={<SearchResults />} />
 
                     {/* Add other Routes here as needed */}
                 </Routes>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {useNavigate} from "react-router-dom";
 
 const SearchBar = () => {
+    const [searchQuery, setSearchQuery] = useState('');
+    const navigate = useNavigate();
+    const handleSearch = async () => {
+      try {
+          const response = await fetch(`http://127.0.0.1:8000/events/search/?q=${encodeURIComponent(searchQuery)}`);
+          const data = await response.json();
+          console.log(data);
+          navigate(`/search-results?q=${encodeURIComponent(searchQuery)}`);
+      } catch (error) {
+          console.error('Error fetching search results', error);
+      }
+    };
     return (
         <div className="flex justify-center mt-16">
             <input
                 type="text"
                 placeholder="Search for events, artists, sports..."
                 className="w-2/4 p-2 rounded-lg text-center text-black"
+                value={searchQuery}
+                onChange={(e) =>setSearchQuery(e.target.value)}
             />
-            <button className="ml-2 p-2 rounded-lg bg-blue-400 text-white">Search</button>
+            <button
+                className="ml-2 p-2 rounded-lg bg-blue-400 text-white"
+                onClick={handleSearch}
+            >
+                Search
+            </button>
         </div>
     );
 };
